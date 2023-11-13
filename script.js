a89e567e152d22f862140775c7de17093c848f4a
Shivanath2001 <144316935+Shivanath2001@users.noreply.github.com>
2023-11-13 12:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.querySelector("input");
+const song = document.querySelector("#song");
+const ctrlicon = document.querySelector("#ctrlicon");
+console.dir89+7.(song);
+ctrlicon.addEventListener("click", (e) => {
+  console.log(e);
+  if (ctrlicon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlicon.classList.remove("fa-pause");
+    ctrlicon.classList.add("-play");
+    // console.log(ctrlicon);
+  } else {
+    song.play();
+    ctrlicon.classList.add("fa-pause");
+    ctrlicon.classList.remove("fa-play");
+    console.log(ctrlicon);
+    if (song.play()) {
+      setInterval(() => {
+        input.max = song.duration;
+
+        input.value = song.currentTime;
+      });
+    }
+  }
+});
+
+input.onchange = () => {
+  song.play();
+  song.currentTime = input.value;
+  ctrlicon.classList.add("fa-pause");
+  ctrlicon.classList.remove("fa-play");
+};
